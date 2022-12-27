b8e4e6f2812fd7a5de8bcc046b3bc32edef3720a
Jkarage <josephbkarage@gmail.com>
2022-12-27 23:50:11 +0300

    Added the joke controller

--- controllers/jokes.go
+++ controllers/jokes.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jkarage/jokes-api/utils"
 )
 
 type Joke struct{}
 
 func (Joke) Home(c *gin.Context) {
 	c.String(http.StatusOK, "Welcome to this jokes api,For more documentaion please visit the /docs endpoint.")
+}
+
+func (Joke) Joke(c *gin.Context) {
+	joke := utils.RandomJoke()
+	c.String(http.StatusOK, "%s", joke)
 }
