621e8e442d2d2f8556402afa6ffa50561d5461e2
Jkarage <josephbkarage@gmail.com>
2023-01-04 22:53:31 +0300

    Update logging

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/jkarage/jokes-api/routes"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jkarage/jokes-api/routes"
+	"github.com/jkarage/jokes-api/utils"
+)
 
 func main() {
+	SetLogOutput()
 	r := routes.InitRoutes()
 	r.Run()
+}
+
+func SetLogOutput() {
+	f, err := os.OpenFile("log/runtime.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	utils.CheckNilErr(err)
+	gin.DefaultWriter = f
 }
--- routes/initRoute.go
+++ routes/initRoute.go
@@ -1,9 +1,19 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jkarage/jokes-api/middlewares"
+)
 
 func InitRoutes() *gin.Engine {
-	r := gin.Default()
+	r := gin.New()
+	r.Use(gin.Recovery(), middlewares.Logger())
 	SetJokesRouter(r)
 	return r
+}
+
+func CheckNil(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
 }
